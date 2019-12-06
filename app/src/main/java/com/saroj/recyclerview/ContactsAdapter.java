93755d08016b094489c789bf730c7b0a2fa43711
sarojpathak <sarojpathak430@gmail.com>
2019-12-06 14:49:11 +0545

    custom Adapter

--- app/src/main/java/com/saroj/recyclerview/ContactsAdapter.java
+++ app/src/main/java/com/saroj/recyclerview/ContactsAdapter.java
@@ -1,13 +1,41 @@
 package com.saroj.recyclerview;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ContactsAdapter {
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
+    List<Contacts> contactsList;
+    Context context;
+
+    public ContactsAdapter(Context context) {
+        this.context = context;
+        this.contactsList=contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder {
         ImageView imgProfile;
         TextView tvName, tvPhone;
